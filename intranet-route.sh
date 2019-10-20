988e06630b27c01be6da007453e9d8305507af62
Pavan Kumar <pavan71198@users.noreply.github.com>
2019-10-20 20:45:30 +0530

    Bug fixes for Ubuntu in route scripts

--- intranet-route.sh
+++ intranet-route.sh
@@ -1,13 +1,31 @@
 echo "Enter the intranet gateway: "
 read gateway
-#Ubuntu
-# sudo route add -net 192.168.0.0/16 gw $gateway metric 600
-# sudo route add -net 172.16.0.0/12 gw $gateway metric 600
-# sudo route add -net 10.0.0.0/8 gw $gateway metric 600
-# ip route show
+# [Ubuntu]
+# Whole Private IP Address Space (be careful 10.8.0.0/24 range might be given to VPN)
+#
+# sudo route add -net 192.168.0.0 netmask 255.255.0.0 gw $gateway metric 600
+# sudo route add -net 172.16.0.0 netmask 255.240.0.0 gw $gateway metric 600
+# sudo route add -net 10.0.0.0 netmask 255.0.0.0 gw $gateway metric 600
+#
+# Only agnigarh firewall
+#
+sudo route add -net 192.168.193.1 netmask 255.255.255.255 gw $gateway metric 600
+#
+# Check final route settings
+#
+ip route show
 
-#MacOS
-sudo route add -net 192.168.0.0/16 $gateway
-sudo route add -net 172.16.0.0/12 $gateway
-sudo route add -net 10.0.0.0/8 $gateway
-netstat -rn
+# [macOS]
+# Whole Private IP Address Space (be careful 10.8.0.0/24 range might be given to VPN)
+#
+# sudo route add -net 192.168.0.0/16 $gateway
+# sudo route add -net 172.16.0.0/12 $gateway
+# sudo route add -net 10.0.0.0/8 $gateway
+#
+# Only agnigarh firewall
+#
+# sudo route add -net 192.168.193.1/32 $gateway
+#
+# Check final route settings
+#
+# netstat -rn
--- reset-route.sh
+++ reset-route.sh
@@ -1,11 +1,29 @@
-#Ubuntu
-# sudo route del -net 202.141.80.0/24 metric 600
-# sudo route del -net 202.141.81.0/24 metric 600
-# sudo route del -net 172.16.0.0/12 metric 600
-# ip route show
+# [Ubuntu]
+# Whole Private IP Address Space (be careful 10.8.0.0/24 range might be given to VPN)
+#
+# sudo route del -net 192.168.0.0/16 netmask 255.255.0.0 metric 600
+# sudo route del -net 172.16.0.0/12 netmask 255.240.0.0 metric 600
+# sudo route del -net 10.0.0.0/8 netmask 255.0.0.0 metric 600
+#
+# Only agnigarh firewall
+#
+sudo route del -net 192.168.193.1 netmask 255.255.255.255 metric 600
+#
+# Check final route settings
+#
+ip route show
 
-#MacOS
-sudo route delete -net 192.168.0.0/16
-sudo route delete -net 172.16.0.0/12
-sudo route delete -net 10.0.0.0/8
-netstat -nr
+# [macOS]
+# Whole Private IP Address Space (be careful 10.8.0.0/24 range might be given to VPN)
+#
+# sudo route delete -net 192.168.0.0/16
+# sudo route delete -net 172.16.0.0/12
+# sudo route delete -net 10.0.0.0/8
+#
+# Only agnigarh firewall
+#
+# sudo route delete -net 192.168.193.1/32
+#
+# Check final route settings
+#
+# netstat -nr
